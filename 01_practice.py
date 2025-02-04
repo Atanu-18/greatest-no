00e5b24de772812dda30c3266efda62424b099fc
ATANU MANDAL <atanu2697@gmail.com>
2025-02-05 01:39:19 +0530

    Add files via upload

---
+++ 01_practice.py
@@ -0,0 +1,17 @@
+a1 = int(input("Enter the 1st no: "))
+a2 = int(input("Enter the 2nd no: "))
+a3 = int(input("Enter the 3rd no: "))
+a4 = int(input("Enter the 4th no: "))
+
+
+if(a1 > a2 and a1 > a3 and a1 > a4):
+    print("the greatest no is: ",a1)
+
+elif(a2 > a1 and a2 > a3 and a2 > a4):
+    print("the greatest no is: ",a2)
+
+elif(a3 > a1 and a3 > a2 and a3 > a4):
+    print("the greatest no is: ",a3)
+
+elif(a4 > a1 and a4 > a2 and a4 > a3):
+    print("the greatest no is: ",a4)
